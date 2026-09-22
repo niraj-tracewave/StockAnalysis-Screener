86a5a341ca1c0390b61dfaf0209abb4221269f84
niraj-tracewave <nirajtv003.tracewave@gmail.com>
2026-09-22 17:34:44 +0530

    fix : Implemented Gross delivery fetch script.

--- app/core/async_celery_utils.py
+++ app/core/async_celery_utils.py
@@ -3982,92 +3982,6 @@ async def fetch_current_day_gross_deliverables_nse_stock_information_async(date_
         db.close()
 
 
-
-async def fetch_current_day_gross_deliverables_bse_stock_information_async():
-    db = SessionLocalSync()
-    try:
-        stmt = (
-                select(CompanyStock)
-                .outerjoin(
-                    KeyDetailsForCS,
-                    CompanyStock.id == KeyDetailsForCS.company_id
-                )
-                .options(selectinload(CompanyStock.details))
-            )
-        file_name = "update_daily_bse_gross_deliverables_data.json"
-        error_symbols = []
-        result = db.execute(stmt)
-        processed_symbols = set(await update_nse_bse_gross_deliverable_data_load_processed_symbols(file_name))
-        new_process_symbol = []
-        unprocessed_companies = []
-        payloads = []
-        skip_symbols = []
-        for c in result.scalars():
-            if c.nse_symbol not in processed_symbols:
-                unprocessed_companies.append(c)
-                if len(unprocessed_companies) == 100:
-                    break
-        started_symbols = [c.nse_symbol for c in unprocessed_companies]
-        await update_nse_bse_gross_deliverable_data_save_processed_symbol(started_symbols, "current_processed_symbols", file_name)
-        for company in unprocessed_companies:
-            try:
-                bse_company_list = await fetch_bse_exact_symbol_data(company.nse_symbol)
-                if not bse_company_list:
-                    skip_symbols.append(company.nse_symbol)
-
-                if bse_company_list:
-                    bse_code = bse_company_list[0].get("bse_code")
-                    c_name = bse_company_list[0].get("company_name")
-                    security_position = await fetch_bse_security_position(bse_code)
-                    security_position = json.loads(security_position)
-                    dt = datetime.strptime(security_position.get("TradeDate"), "%d %b %Y |%H:%M")
-
-                    formatted_tradedate = dt.strftime("%Y-%m-%d %H:%M:%S")
-                    bsePriceVolumeDeliverable = {
-                        "dt_tm": formatted_tradedate,
-                        "Scrip_cd": bse_code,
-                        "LONG_NAME": c_name,
-                        "Delivery_Qty": parse_number(security_position.get("DeliverableQty")),
-                        "Delivery_Val": None,
-                        "No_Of_Shares": parse_number(security_position.get("QtyTraded")),
-                        "Turnover": None,
-                        "Perc_Del_Qty": parse_number(security_position.get("PcDQ_TQ")),
-                    }
-
-                    payloads.append({
-                        "company_id": company.id,
-                        "symbol": company.nse_symbol,
-                        "bse": bsePriceVolumeDeliverable,
-                    })
-                    new_process_symbol.append(company.nse_symbol)
-
-                    if len(payloads) == GROUP_SIZE:
-                        group(
-                            process_delivery_batch.s(payloads.copy(), skip_symbols.copy(), file_name),
-                        ).apply_async()
-                        payloads.clear()
-                        skip_symbols.clear()
-
-            except Exception as symbol_error:
-                db.rollback()
-                error_symbols.append(company.nse_symbol)
-                print(f"Error for symbol {company.nse_symbol}: {symbol_error}")
-                continue
-
-        if payloads:
-            group(
-                process_delivery_batch.s(
-                    payloads.copy(),skip_symbols.copy(),
-                    file_name
-                ),
-            ).apply_async()
-            payloads.clear()
-            skip_symbols.clear()
-
-    finally:
-        db.close()
-
-
 @shared_task(bind=True)
 def nse_process_delivery_batch(self, batch, skip_symbols, file_name):
 
@@ -4116,6 +4030,100 @@ def nse_process_delivery_batch(self, batch, skip_symbols, file_name):
         if total_count == total_count_from_file:
             update_nse_bse_gross_deliverable_list_load_processed_symbols(file_name)
         db.close()
+
+async def fetch_current_day_gross_deliverables_bse_stock_information_async():
+    db = SessionLocalSync()
+    try:
+        stmt = (
+            select(CompanyStock)
+            .outerjoin(
+                KeyDetailsForCS,
+                CompanyStock.id == KeyDetailsForCS.company_id
+            )
+            .options(selectinload(CompanyStock.details))
+        )
+        now_ist = datetime.now(ZoneInfo("Asia/Kolkata"))
+        date_str = now_ist.strftime("%Y%m%d")
+        redis_target = f"redis:bse_gross_deliverables:{date_str}"
+
+        error_symbols = []
+        result = db.execute(stmt)
+        processed_symbols = set(await update_nse_bse_gross_deliverable_data_load_processed_symbols(redis_target))
+        new_process_symbol = []
+        unprocessed_companies = []
+        payloads = []
+        skip_symbols = []
+        for c in result.scalars():
+            if c.nse_symbol not in processed_symbols:
+                unprocessed_companies.append(c)
+                if len(unprocessed_companies) == 100:
+                    break
+
+        if not unprocessed_companies:
+            print(f"All BSE symbols have already been processed for date {date_str}.")
+            return
+
+        started_symbols = [c.nse_symbol for c in unprocessed_companies]
+        await update_nse_bse_gross_deliverable_data_save_processed_symbol(started_symbols, "current_processed_symbols", redis_target)
+        for company in unprocessed_companies:
+            try:
+                bse_company_list = await fetch_bse_exact_symbol_data(company.nse_symbol)
+                if not bse_company_list:
+                    skip_symbols.append(company.nse_symbol)
+
+                if bse_company_list:
+                    bse_code = bse_company_list[0].get("bse_code")
+                    c_name = bse_company_list[0].get("company_name")
+                    security_position = await fetch_bse_security_position(bse_code)
+                    security_position = json.loads(security_position)
+                    dt = datetime.strptime(security_position.get("TradeDate"), "%d %b %Y |%H:%M")
+
+                    formatted_tradedate = dt.strftime("%Y-%m-%d %H:%M:%S")
+                    bsePriceVolumeDeliverable = {
+                        "dt_tm": formatted_tradedate,
+                        "Scrip_cd": bse_code,
+                        "LONG_NAME": c_name,
+                        "Delivery_Qty": parse_number(security_position.get("DeliverableQty")),
+                        "Delivery_Val": None,
+                        "No_Of_Shares": parse_number(security_position.get("QtyTraded")),
+                        "Turnover": None,
+                        "Perc_Del_Qty": parse_number(security_position.get("PcDQ_TQ")),
+                    }
+
+                    payloads.append({
+                        "company_id": company.id,
+                        "symbol": company.nse_symbol,
+                        "bse": bsePriceVolumeDeliverable,
+                    })
+                    new_process_symbol.append(company.nse_symbol)
+
+                    if len(payloads) == GROUP_SIZE:
+                        group(
+                            nse_process_delivery_batch.s(payloads.copy(), skip_symbols.copy(), redis_target),
+                        ).apply_async()
+                        payloads.clear()
+                        skip_symbols.clear()
+
+            except Exception as symbol_error:
+                db.rollback()
+                error_symbols.append(company.nse_symbol)
+                print(f"Error for symbol {company.nse_symbol}: {symbol_error}")
+                continue
+
+        if payloads or skip_symbols:
+            group(
+                nse_process_delivery_batch.s(
+                    payloads.copy(), skip_symbols.copy(),
+                    redis_target
+                ),
+            ).apply_async()
+            payloads.clear()
+            skip_symbols.clear()
+
+    finally:
+        db.close()
+
+
 
 async def hourly_fetch_current_day_gross_deliverables_nse_stock_information_async():
     db = SessionLocalSync()
